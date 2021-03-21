2bc162e3e6004e1b4b61b95e0ee726a80cadc24b
hoanglm-2435 <le.minh.hoang@sun-asterisk.com>
2021-03-22 00:00:22 +0700

    update: improve shopee product spider

--- crawl_service/spiders/product.py
+++ crawl_service/spiders/product.py
@@ -6,23 +6,74 @@ from scrapy_splash import SplashRequest
 class CrawlServiceItem(scrapy.Item):
     product_name = scrapy.Field()
     price = scrapy.Field()
-    price_sale = scrapy.Field()
+    stock_available = scrapy.Field()
     sold_count = scrapy.Field()
-    location = scrapy.Field()
+    reviews = scrapy.Field()
+    rating = scrapy.Field()
 
 
 class ProductSpider(scrapy.Spider):
     name = "product"
     allowed_domains = ["shopee.vn"]
 
-    start_urls = ["https://shopee.vn/search?keyword=sneakers&sortBy=sales"]
+    start_urls = ["https://shopee.vn/shop/151338284/search"]
 
-    script = '''
+    render_script = '''
     function main(splash)
+        splash:init_cookies(splash.args.cookies)
+        local num_scrolls = 10
+        local scroll_delay = 1
+
+        local scroll_to = splash:jsfunc("window.scrollTo")
+        local get_body_height = splash:jsfunc(
+            "function() {return document.body.scrollHeight;}"
+        )
         assert(splash:go(splash.args.url))
-        assert(splash:wait(5))
+
+        assert(splash:wait(2))
+        for _ = 1, num_scrolls do
+            local height = get_body_height()
+            for i = 1, 10 do
+                scroll_to(0, height * i/10)
+                splash:wait(scroll_delay/10)
+            end
+        end  
+        assert(splash:wait(2))
+
+        return {
+            cookies = splash:get_cookies(),
+            html = splash:html(),
+            url = splash:url(),
+        }
+    end
+    '''
+
+    pagination_script = '''
+    function main(splash)
+        splash:init_cookies(splash.args.cookies)
+        local num_scrolls = 10
+        local scroll_delay = 1
+
+        local scroll_to = splash:jsfunc("window.scrollTo")
+        local get_body_height = splash:jsfunc(
+            "function() {return document.body.scrollHeight;}"
+        )
+        assert(splash:go(splash.args.url))
+        assert(splash:wait(2))
+        assert(splash:runjs("document.querySelector('button.shopee-icon-button.shopee-icon-button--right').click()"))
+        assert(splash:wait(2))
+
+        for _ = 1, num_scrolls do
+            local height = get_body_height()
+            for i = 1, 10 do
+                scroll_to(0, height * i/10)
+                splash:wait(scroll_delay/10)
+            end
+        end  
+        assert(splash:wait(2))
         
         return {
+            cookies = splash:get_cookies(),
             html = splash:html(),
             url = splash:url(),
         }
@@ -36,63 +87,29 @@ class ProductSpider(scrapy.Spider):
                 endpoint="render.html",
                 callback=self.parse,
                 args={
-                    'wait': 5,
-                    "lua_source": self.script,
+                    'wait': 2,
                     'viewport': '3964x3964',
                 },
                 dont_filter=True
             )
 
     def parse(self, response):
-        for item_url in response.css("div.shopee-search-item-result__item > a ::attr(href)").extract():
+        list_url = response.css(
+            "div.shop-search-result-view__item > a ::attr(href)").extract()
+        for item_url in list_url:
             yield SplashRequest(
                 url=response.urljoin(item_url),
                 endpoint="render.html",
                 callback=self.parse_product,
                 args={
-                    'wait': 5,
-                    "lua_source": self.script,
+                    'wait': 2,
+                    "lua_source": self.render_script,
                     'viewport': '3964x3964',
                 },
                 dont_filter=True,
             )
 
-        next_page = response.css("div.next > a ::attr(href)").extract_first()
-        if next_page:
-            yield SplashRequest(
-                url=response.urljoin(next_page),
-                endpoint="render.html",
-                callback=self.parse_product,
-                args={
-                    'wait': 5,
-                    "lua_source": self.script,
-                    'viewport': '3964x3964',
-                },
-                dont_filter=True,
-            )
-
-    def parse_product(self, response):
-        item = CrawlServiceItem()
-
-        item["product_name"] = response.css(
-            "div.attM6y span ::text").extract_first()
-        # item["price"] = data.css(
-        #     "div.WTFwws._1lK1eK._5W0f35 span:last-child ::text").extract_first()
-        # if data.css("div.WTFwws._3f05Zc._3_-SiN"):
-        #     item["price"] = data.css(
-        #         "div.WTFwws._3f05Zc._3_-SiN ::text").extract_first()
-        #     item["price_sale"] = data.css(
-        #         "div.WTFwws._1lK1eK._5W0f35 span:last-child ::text").extract_first()
-        # else:
-        #     item["price"] = data.css(
-        #         "div.WTFwws._1lK1eK._5W0f35 > span._29R_un:last-child ::text").extract_first()
-        #     item["price_sale"] = data.css(
-        #         "div.WTFwws._1lK1eK._5W0f35 > span._29R_un:nth-child(2) ::text").extract_first()
-        # item["sold_count"] = data.css(
-        #     "div.go5yPW ::text").extract_first()
-        # item["location"] = data.css(
-        #     "div._2CWevj ::text").extract_first()
-        yield item
+        print("NEXT PAGE")
 
         yield SplashRequest(
             url=response.url,
@@ -101,12 +118,30 @@ class ProductSpider(scrapy.Spider):
                 "splash": {
                     "endpoint": "execute",
                     "args": {
-                        'wait': 5,
+                        'wait': 2,
                         'url': response.url,
-                        "lua_source": self.script,
+                        "lua_source": self.pagination_script,
                         'viewport': '3964x3964',
                     },
                 }
             },
             dont_filter=True
         )
+
+    def parse_product(self, response):
+        item = CrawlServiceItem()
+
+        item["product_name"] = response.css(
+            "div.attM6y > span ::text").extract_first()
+        item["price"] = response.css(
+            "div._3e_UQT ::text").extract_first()
+        item["rating"] = response.css(
+            "div.OitLRu._1mYa1t ::text").extract_first()
+        item["reviews"] = response.css(
+            "div.flex._21hHOx > div:nth-child(2) > div.OitLRu ::text").extract_first()
+        item["stock_available"] = response.css(
+            "div._1afiLm > div:nth-child(3) > div ::text").extract_first()
+        item["sold_count"] = response.css(
+            "div.aca9MM ::text").extract_first()
+
+        yield item
