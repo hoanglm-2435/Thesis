66191756986af2fb0dc05b8b8053a82378c713cb
hoanglm-2435 <le.minh.hoang@sun-asterisk.com>
2021-04-06 00:46:44 +0700

    Crawl comments of each product using the request lib

--- crawl_service/queries/readdata.py
+++ crawl_service/queries/readdata.py
@@ -19,3 +19,21 @@ def dataReader(spiderName):
         return dict(list)
     except MySQLdb.Error as e:
         print("Error reading data from MySQL table", e)
+
+def getProducts(spiderName):
+    try:
+        conn = MySQLdb.connect(
+            'localhost',
+            'root',
+            'hoangminh99',
+            'shopee_crawler',
+            charset="utf8",
+            use_unicode=True,
+        )
+        cursor = conn.cursor()
+        cursor.execute('''SELECT id, url, reviews FROM `{0}`;'''.format(spiderName))
+        list = [(item[0], item[1], item[2]) for item in cursor.fetchall()]
+
+        return list
+    except MySQLdb.Error as e:
+        print("Error reading data from MySQL table", e)
